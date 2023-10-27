1c2a4ae451a7c3b62efde7ce354500708070b68d
Kasper Bankler <kasperbankler@gmail.com>
2023-10-27 20:57:35 +0200

    add github button and fix popup layout

--- LectioGames/popup.js
+++ LectioGames/popup.js
@@ -13,10 +13,13 @@ document.querySelector('#go-to-options').addEventListener('click', function () {
     }
 });
 
+document.querySelector('#github').addEventListener('click', function () {
+    chrome.tabs.create({ url: "https://github.com/Kasper-Bankler" });
+});
+
 
 async function getCurrentTab() {
     let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
 }
